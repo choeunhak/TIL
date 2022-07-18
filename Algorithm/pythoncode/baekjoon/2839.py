8e10ab1e4fe09202d45d57be99ea49c9cfa5da48
조은학 <02069@naver.com>
2022-07-18 18:32:25 +0900

    백준 2839 dp 새로운 풀이 elif 제거

--- Algorithm/pythoncode/baekjoon/2839.py
+++ Algorithm/pythoncode/baekjoon/2839.py
@@ -19,3 +19,22 @@ print(minValue)
 
 
 #https://velog.io/@y7y1h13/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98%EB%B0%B1%EC%A4%80-2839%EB%B2%88-%EC%84%A4%ED%83%95-%EB%B0%B0%EB%8B%ACdppython
+
+#아래 풀이도 통과함
+# 3보다 5가 더 작은 값을 주기 때문인듯
+# def solution(N):
+#     sugar = [-1] * 5001
+#     sugar[3] = sugar[5] = 1
+
+#     for i in range(6, N + 1):
+#         if i % 3 == 0:
+#             sugar[i] = sugar[i - 3] + 1
+#         if i % 5 == 0:
+#             sugar[i] = sugar[i - 5] + 1
+#         elif sugar[i - 3] > 0 and sugar[i - 5] > 0:
+#             sugar[i] = min(sugar[i - 3], sugar[i - 5]) + 1
+#     print(sugar[N])
+
+
+# if __name__ == "__main__":
+#     solution(N = int(input()))

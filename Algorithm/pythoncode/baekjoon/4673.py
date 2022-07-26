11249836a76bbccd16e0006698c8ce73080706d8
조은학 <02069@naver.com>
2022-07-26 17:26:41 +0900

    백준 4673

--- Algorithm/pythoncode/baekjoon/4673.py
+++ Algorithm/pythoncode/baekjoon/4673.py
@@ -1,4 +1,22 @@
-def solution():
-    for i in range(10000):
-        stri=str(i)
-        for i in range(len(stri)):
+def d(n):
+    n=str(n)
+    tmp=0
+    for i in range(len(n)):
+        tmp=tmp+int(n[i])
+    result=tmp+int(n)
+    return result
+
+tmp=[]
+for i in range(10001):
+    tmp.append(i)
+
+d_num=[]
+for i in range(10001):
+    d_num.append(d(i))
+self_num = set(tmp)-set(d_num)
+
+self_num=list(self_num)
+
+self_num.sort()
+for i in self_num:
+    print(i)

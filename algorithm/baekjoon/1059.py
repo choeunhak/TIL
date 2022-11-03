5deda24759c1520850921c4980027a4b7dbbf277
choeunhak <02069@naver.com>
2022-11-03 11:34:55 +0900

    Create 1059.py

---
+++ algorithm/baekjoon/1059.py
@@ -0,0 +1,35 @@
+import itertools
+
+l = int(input())
+nums = list(map(int, input().split()))
+n = int(input())
+
+cand = []
+start = 0
+tmp_nums = []
+nums.sort()
+cnt = 0
+
+if (n < nums[0]):
+  nums.insert(0, 0)
+elif (len(nums) == 1):
+  nums.insert(0, 0)
+
+if (n in nums):
+  cnt = 0
+else:
+  for i in range(len(nums)):
+    if (nums[i] > n):
+      start = i - 1
+      break
+  for i in range(nums[start] + 1, nums[start + 1]):
+    tmp_nums.append(i)
+
+  cand = itertools.combinations(tmp_nums, 2)
+  cand = list(cand)
+
+  for i, j in cand:
+    if (i <= n and n <= j):
+      cnt = cnt + 1
+# print(cand)
+print(cnt)

0cb957d8fa2ac7be039f7c10ebfc4c6abfffe854
choeunhak <02069@naver.com>
2023-01-09 18:19:57 +0900

    defaultdict 사용법 추가

--- algorithm/boilerplate/dict.py
+++ algorithm/boilerplate/dict.py
@@ -6,3 +6,11 @@ sorted_dict = sorted(dict.items(), key=lambda x: x[1])
 
 # value로 내림차순 정렬
 sorted_dict = sorted(dict.items(), key=lambda x: x[1], reverse=True)
+
+
+# defaultdict
+# 없는 키여도 keyerror를 일으키지 않는다.
+from collections import defaultdict
+
+# 아래와 같이 쓸경우 없는 키일때 list를 반환!
+ex_dict = defaultdict(list)

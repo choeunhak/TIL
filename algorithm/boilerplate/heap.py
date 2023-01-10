9bf8124f4bff1dc57a90f0346e79a648595f22f2
choeunhak <02069@naver.com>
2023-01-11 01:12:38 +0900

    힙 추가

---
+++ algorithm/boilerplate/heap.py
@@ -0,0 +1,19 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+
+# 최소힙
+heap = []
+for i in range(n):
+    x = int(input())
+    if(x==0):
+        # 원소 삭제
+        if(heap):
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        # 원소 추가
+        heapq.heappush(heap, x)
